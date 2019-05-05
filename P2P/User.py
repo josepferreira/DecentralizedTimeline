495f163208dcb503cd0e6e8440addf68e625cc7f
nunojrsilva <nunojosersilva@gmail.com>
2019-05-05 16:19:10 +0100

    Update User.py on get_ip()

--- P2P/User.py
+++ P2P/User.py
@@ -163,6 +163,7 @@ def get_utilizador():
     username = username.replace('\n', '')
 
 def get_ip():
+    global ip
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     ip = s.getsockname()[0]
