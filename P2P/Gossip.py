6f1fd88715dca59b55f6e3083c51f986a00c7dc9
nunojrsilva <nunojosersilva@gmail.com>
2019-05-15 17:43:11 +0100

    Adiciona gossip e faltaFazer.txt

---
+++ P2P/Gossip.py
@@ -0,0 +1,28 @@
+import random, math
+
+from Socket.MySocket import utilizador_online
+
+def selecionaAleatorio(utilizadores,numero_envia):
+    tam = len(utilizadores)
+    k = math.ceil(math.log(numero_envia))#+16 #garante 99.9999887465% de probabilidade de chegar a todos
+    # segundo o artigo do cuckoo
+    if k > tam:
+        k = tam
+    envia = []
+    random.shuffle(utilizadores)
+    quantos = 0
+
+    for (i,v) in utilizadores:
+        #verifica se esta online
+        online = utilizador_online(v[0],v[1])
+        if online:
+            envia.append((i,v))
+            quantos += 1
+            if quantos >= k:
+                break            
+
+    return (envia,[i for i in utilizadores if i not in envia])
+
+def cria_mensagem(mensagem,faltam):
+    mensagem['faltam'] = faltam
+    return mensagem
